2df5ac985334b97f4e600d017a317187fedec22d
SanchitKumbhar <sanchitskumbhar@gmail.com>
2026-02-25 17:08:31 +0530

    forget pass

--- router/users.routes.js
+++ router/users.routes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const { createUser, getUsers, getUserById, updateUser, deleteUser, verifyEmail, resendVerification } = require('../user/user.Controller');
 const { loginUser, logout, forgotPassword, resetPassword } = require('../user/auth.Controller');
-const { createProfile: createStudentProfile, getProfiles: getStudentProfiles, getProfileById: getStudentProfileById, updateProfile: updateStudentProfile, deleteProfile: deleteStudentProfile } = require('../user/studentProfile.Controller');
 
 const token__middleware = require('../middleware/jwttoken.middleware');
 
@@ -25,18 +24,7 @@ router.get('/users/:id', token__middleware, getUserById);
 router.put('/users/:id', token__middleware, updateUser);
 router.delete('/users/:id', token__middleware, deleteUser);
 
-// Student profiles
-router.post('/student-profiles', createStudentProfile);
-router.get('/student-profiles', token__middleware, getStudentProfiles);
-// Convenience endpoints that return/modify the authenticated user's profile
-router.get('/student-profiles/me', token__middleware, (req, res, next) => { req.params.id = 'me'; next(); }, getStudentProfileById);
-router.put('/student-profiles/me', token__middleware, (req, res, next) => { req.params.id = 'me'; next(); }, updateStudentProfile);
-router.delete('/student-profiles/me', token__middleware, (req, res, next) => { req.params.id = 'me'; next(); }, deleteStudentProfile);
-router.get('/student-profiles/:id', token__middleware, getStudentProfileById);
-router.put('/student-profiles/:id', token__middleware, updateStudentProfile);
-router.delete('/student-profiles/:id', token__middleware, deleteStudentProfile);
-
-
+ 
 module.exports = router;
 
 
